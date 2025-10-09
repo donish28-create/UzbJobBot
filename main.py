2938c6a8a017d1250a55486f8231a913562d5ba2
donish28-create <donish28@gmail.com>
2025-10-09 17:01:51 +0500

    Update main.py

--- main.py
+++ main.py
@@ -21,12 +21,94 @@ router = Router()
 
 # -------------------- HUDUDLAR --------------------
 REGIONS = {
-    "Toshkent shahri": ["Bektemir", "Chilonzor", "Mirzo Ulug‘bek", "Mirobod", "Olmazor", "Sergeli", "Shayxontohur", "Uchtepa", "Yakkasaroy", "Yashnobod", "Yunusobod"],
-    "Toshkent viloyati": ["Bekobod", "Bo‘ka", "Chinoz", "Ohangaron", "Parkent", "Piskent", "Qibray", "Quyi Chirchiq", "O‘rta Chirchiq", "Yangiyo‘l", "Zangiota", "Toshkent tumani"],
-    "Andijon": ["Andijon shahar", "Asaka", "Baliqchi", "Buloqboshi", "Izboskan", "Jalaquduq", "Marhamat", "Oltinko‘l", "Paxtaobod", "Shahrixon", "Ulug‘nor", "Xo‘jaobod"],
-    "Farg‘ona": ["Farg‘ona shahar", "Oltiariq", "Bag‘dod", "Beshariq", "Dang‘ara", "Furqat", "Quva", "Qo‘qon", "Rishton", "So‘x", "Toshloq", "Uchko‘prik", "Yozyovon"],
-    "Namangan": ["Namangan shahar", "Chortoq", "Chust", "Kosonsoy", "Mingbuloq", "Norin", "Pop", "To‘raqo‘rg‘on", "Uchqo‘rg‘on", "Yangiqo‘rg‘on"],
-    "Samarqand": ["Samarqand shahar", "Bulung‘ur", "Ishtixon", "Jomboy", "Kattaqo‘rg‘on", "Narpay", "Nurobod", "Oqdaryo", "Paxtachi", "Pastdarg‘om", "Payariq", "Qo‘shrabot", "Toyloq", "Urgut"],
+    "Butun Oʻzbekiston boʻyicha": [],
+
+    "Toshkent shahri": [
+        "Bektemir tumani", "Chilonzor tumani", "Yashnobod tumani", "Mirobod tumani",
+        "Mirzo Ulugʻbek tumani", "Sergeli tumani", "Shayxontohur tumani", "Olmazor tumani",
+        "Uchtepa tumani", "Yakkasaroy tumani", "Yunusobod tumani"
+    ],
+
+    "Toshkent viloyati": [
+        "Bekobod tumani", "Boʻstonliq tumani", "Boʻka tumani", "Chinoz tumani", "Qibray tumani",
+        "Ohangaron tumani", "Oqqoʻrgʻon tumani", "Parkent tumani", "Piskent tumani",
+        "Quyi Chirchiq tumani", "Oʻrta Chirchiq tumani", "Yangiyoʻl tumani",
+        "Yuqori Chirchiq tumani", "Zangiota tumani"
+    ],
+
+    "Andijon": [
+        "Andijon tumani", "Asaka tumani", "Baliqchi tumani", "Boʻston tumani", "Buloqboshi tumani",
+        "Izboskan tumani", "Jalaquduq tumani", "Xoʻjaobod tumani", "Qoʻrgʻontepa tumani",
+        "Marhamat tumani", "Oltinkoʻl tumani", "Paxtaobod tumani", "Shahrixon tumani",
+        "Ulugʻnor tumani"
+    ],
+
+    "Fargʻona": [
+        "Oltiariq tumani", "Bagʻdod tumani", "Beshariq tumani", "Buvayda tumani", "Dangʻara tumani",
+        "Fargʻona tumani", "Furqat tumani", "Qoʻshtepa tumani", "Quva tumani", "Rishton tumani",
+        "Soʻx tumani", "Toshloq tumani", "Uchkoʻprik tumani", "Oʻzbekiston tumani", "Yozyovon tumani"
+    ],
+
+    "Namangan": [
+        "Chortoq tumani", "Chust tumani", "Kosonsoy tumani", "Mingbuloq tumani",
+        "Namangan tumani", "Norin tumani", "Pop tumani", "Toʻraqoʻrgʻon tumani",
+        "Uchqoʻrgʻon tumani", "Uychi tumani", "Yangiqoʻrgʻon tumani"
+    ],
+
+    "Samarqand": [
+        "Bulungʻur tumani", "Ishtixon tumani", "Jomboy tumani", "Kattaqoʻrgʻon tumani",
+        "Qoʻshrabot tumani", "Narpay tumani", "Nurobod tumani", "Oqdaryo tumani",
+        "Paxtachi tumani", "Payariq tumani", "Pastdargʻom tumani", "Samarqand tumani",
+        "Toyloq tumani", "Urgut tumani"
+    ],
+
+    "Buxoro": [
+        "Olot tumani", "Buxoro tumani", "Gʻijduvon tumani", "Jondor tumani", "Kogon tumani",
+        "Qorakoʻl tumani", "Qorovulbozor tumani", "Peshku tumani", "Romitan tumani",
+        "Shofirkon tumani", "Vobkent tumani"
+    ],
+
+    "Navoiy": [
+        "Konimex tumani", "Karmana tumani", "Qiziltepa tumani", "Xatirchi tumani",
+        "Navbahor tumani", "Nurota tumani", "Tomdi tumani", "Uchquduq tumani"
+    ],
+
+    "Xorazm": [
+        "Bogʻot tumani", "Gurlan tumani", "Xonqa tumani", "Hazorasp tumani", "Xiva tumani",
+        "Qoʻshkoʻpir tumani", "Shovot tumani", "Urganch tumani", "Yangiariq tumani",
+        "Yangibozor tumani", "Tuproqqalʼa tumani"
+    ],
+
+    "Qashqadaryo": [
+        "Chiroqchi tumani", "Dehqonobod tumani", "Gʻuzor tumani", "Qamashi tumani",
+        "Qarshi tumani", "Koson tumani", "Kasbi tumani", "Kitob tumani", "Mirishkor tumani",
+        "Muborak tumani", "Nishon tumani", "Shahrisabz tumani", "Yakkabogʻ tumani",
+        "Koʻkdala tumani"
+    ],
+
+    "Surxondaryo": [
+        "Angor tumani", "Boysun tumani", "Denov tumani", "Jarqoʻrgʻon tumani", "Qiziriq tumani",
+        "Qumqoʻrgʻon tumani", "Muzrabot tumani", "Oltinsoy tumani", "Sariosiyo tumani",
+        "Sherobod tumani", "Shoʻrchi tumani", "Termiz tumani", "Uzun tumani", "Bandixon tumani"
+    ],
+
+    "Jizzax": [
+        "Arnasoy tumani", "Baxmal tumani", "Doʻstlik tumani", "Forish tumani", "Gʻallaorol tumani",
+        "Sharof Rashidov tumani", "Mirzachoʻl tumani", "Paxtakor tumani", "Yangiobod tumani",
+        "Zomin tumani", "Zafarobod tumani", "Zarbdor tumani"
+    ],
+
+    "Sirdaryo": [
+        "Oqoltin tumani", "Boyovut tumani", "Guliston tumani", "Xovos tumani", "Mirzaobod tumani",
+        "Sayxunobod tumani", "Sardoba tumani", "Sirdaryo tumani"
+    ],
+
+    "Qoraqalpogʻiston R.": [
+        "Amudaryo tumani", "Beruniy tumani", "Chimboy tumani", "Ellikqalʼa tumani", "Kegeyli tumani",
+        "Moʻynoq tumani", "Nukus tumani", "Qanlikoʻl tumani", "Qoʻngʻirot tumani", "Qoraoʻzak tumani",
+        "Shumanay tumani", "Taxtakoʻpir tumani", "Toʻrtkoʻl tumani", "Xoʻjayli tumani",
+        "Taxiatosh tumani", "Boʻzatov tumani"
+    ]
 }
 
 # -------------------- Kategoriyalar --------------------
