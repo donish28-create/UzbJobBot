f35eae11be39d4976afdaefeea7d3b9be601a47b
donish28-create <donish28@gmail.com>
2025-10-15 10:50:36 +0500

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,39 @@
-from database import db_init
+import aiosqlite
+
+DB_NAME = "data.db"
+
+async def db_init():
+    async with aiosqlite.connect(DB_NAME) as db:
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS seekers (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER,
+            full_name TEXT,
+            category TEXT,
+            region TEXT,
+            district TEXT,
+            experience TEXT,
+            salary TEXT,
+            contact TEXT,
+            extra TEXT,
+            created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+        """)
+
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS employers (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER,
+            category TEXT,
+            region TEXT,
+            district TEXT,
+            salary TEXT,
+            contact TEXT,
+            extra TEXT,
+            created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+        """)
+
+        await db.commit()
+        print("📁 Database initialized successfully.")
+
